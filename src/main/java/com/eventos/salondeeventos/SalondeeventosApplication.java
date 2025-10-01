ac3cac976f15ea0afe040d0ea9286726ab10222e
jessica soria <jessisoria72@gmail.com>
2025-10-01 20:53:23 -0300

    sistema de reserva de salon de eventos

---
+++ src/main/java/com/eventos/salondeeventos/SalondeeventosApplication.java
@@ -0,0 +1,13 @@
+package com.eventos.salondeeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalondeeventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalondeeventosApplication.class, args);
+	}
+
+}
